ede2efd70110e050e5335558c65b762ad325c238
DevMinded <keles490@googlemail.com>
2025-03-31 16:28:01 +0200

    overlay functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
-let pics = ["0","1","2","3","4","5","6","7","8","9","10","11"]
-
+let pics = ["0","1","2","3","4","5","6","7","8","9","10","11"];
+let actualIndex = 0;
 
 function render(i){
     let picBox = document.getElementById("pics");
@@ -13,25 +13,26 @@ function render(i){
 function getIMG(index){
 
     return `
-        <img onclick="render_overlay()" id="${index}" src="./img/${index}.jpg" alt="">`
+        <img onclick="render_overlay(${index})" class="pictures" id="${index}" src="./img/${index}.jpg" alt="">`
     }
 
 
-function render_overlay(){
+function render_overlay(index){
+    actualIndex = index
     clearOverlay();
     toggleOverlay();
     let overlayBox = document.getElementById("overlay_section");
-    overlayBox.innerHTML += showOverlay();
+    overlayBox.innerHTML += showOverlay(actualIndex);
 }
 
-function showOverlay(){
+function showOverlay(index){
     
     return `
             <img onclick="toggleOverlay()" class="close_overlay" src="./img/x.png" alt="">
             <div>
-            <img class="arrows" src="./img/arrow-left.png" alt="">
-            <img id="overlay_pic" src="./img/1.jpg" alt="">
-            <img class="arrows" src="./img/arrow-right.png" alt="">
+            <img onclick="prevPic()" class="arrows" src="./img/arrow-left.png" alt="">
+            <img id="overlay_pic" src="./img/${index}.jpg" alt="">
+            <img onclick="nextPic()" class="arrows" src="./img/arrow-right.png" alt="">
         </div>`
 }
 
@@ -41,4 +42,31 @@ function toggleOverlay(){
 
 function clearOverlay(){
     overlayEmpty = document.getElementById("overlay_section").innerHTML = "";
+}
+
+function nextPic(){
+    if (actualIndex < pics.length){
+        actualIndex++;
+        FirstLastPic()
+        updateOverlay(actualIndex);
+    }
+}
+function prevPic(){
+    if (actualIndex < pics.length){
+        actualIndex--;
+        FirstLastPic()
+        updateOverlay(actualIndex);
+    }
+}
+
+function FirstLastPic(){
+    if (actualIndex = pics.length){
+        actualIndex = 0;
+    }else if(actualIndex < pics.length){
+        actualIndex = pics.length-1;
+    }
+}
+
+function updateOverlay(){
+    document.getElementById("overlay_pic").src = `./img/${actualIndex}.jpg`;
 }
