fb7c2d1d3d517811ad29f4ba5b7977d94e3466db
DevMinded <keles490@googlemail.com>
2025-03-31 12:19:49 +0200

    overlay + functions

--- script.js
+++ script.js
@@ -13,5 +13,32 @@ function render(i){
 function getIMG(index){
 
     return `
-        <img id="${index}" src="./img/${index}.jpg" alt="">`
+        <img onclick="render_overlay()" id="${index}" src="./img/${index}.jpg" alt="">`
     }
+
+
+function render_overlay(){
+    clearOverlay();
+    toggleOverlay();
+    let overlayBox = document.getElementById("overlay_section");
+    overlayBox.innerHTML += showOverlay();
+}
+
+function showOverlay(){
+    
+    return `
+            <img onclick="toggleOverlay()" class="close_overlay" src="./img/x.png" alt="">
+            <div>
+            <img class="arrows" src="./img/arrow-left.png" alt="">
+            <img id="overlay_pic" src="./img/1.jpg" alt="">
+            <img class="arrows" src="./img/arrow-right.png" alt="">
+        </div>`
+}
+
+function toggleOverlay(){
+    document.getElementById("overlay_section").classList.toggle('d-none');
+}
+
+function clearOverlay(){
+    overlayEmpty = document.getElementById("overlay_section").innerHTML = "";
+}
